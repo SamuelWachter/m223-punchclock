a0180ede301f298b399f79b6cba9732b6a81e83e
SamuelWachter <wachtersamuel8@gmail.com>
2023-08-29 13:52:00 +0200

    delete

--- src/main/java/ch/zli/m223/service/EntryService.java
+++ src/main/java/ch/zli/m223/service/EntryService.java
@@ -24,4 +24,17 @@ public class EntryService {
         var query = entityManager.createQuery("FROM Entry", Entry.class);
         return query.getResultList();
     }
+
+    @Transactional
+    public void deleteEntry(Long id){
+        var entity = entityManager.find(Entry.class, id);
+        entityManager.remove(entity);
+    }
+
+    @Transactional
+    public Entry updateEntry(Long id, Entry entry){
+        entry.setId(id);
+        return entityManager.merge(entry);
+    }
+
 }
